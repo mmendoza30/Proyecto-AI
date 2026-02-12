2441fa464fe6aefb5ee2f45798e21203faa2cec6
wedellXrozcoG <worozco1604@gmail.com>
2026-02-12 10:36:41 -0600

    Creación de funciones de preprocesamientos, graficos, modelo y notebooks(01_EDA,02_Preprocesamiento,03_ANN_Modelo1)

---
+++ src/data_prep.py
@@ -0,0 +1,151 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import os
+
+class ProcesadoEDA:
+    def __init__(self, df):
+        self.df = df
+
+    def mostrar_head(self):
+        """Devuelve las primeras 5 filas del DataFrame"""
+        return self.df.head()
+
+    def mostrar_info(self):
+        """Devuelve la información general del DataFrame"""
+        return self.df.info()
+
+    def mostrar_dimensiones(self):
+        """Devuelve las dimensiones del DataFrame"""
+        return self.df.shape
+
+    def mostrar_valores_nulos(self):
+        """Devuelve la cantidad de valores nulos por columna"""
+        return self.df.isnull().sum()
+
+    def resum_transpose(self):
+        """Principales datos estadísticos del dataframe"""
+        return self.df.describe().transpose()
+
+    def ch_bool(self):
+        """Cambia datos binarios Yes/No a 1 y 0"""
+        columnas_binarias = [
+            'schoolsup', 'famsup', 'paid', 'activities',
+            'nursery', 'higher', 'internet', 'romantic'
+        ]
+        self.df[columnas_binarias] = self.df[columnas_binarias].replace({'yes': 1, 'no': 0})
+        self.df['famsize'] = self.df['famsize'].replace({'LE3': 1, 'GT3': 0})
+        self.df['sex'] = self.df['sex'].replace({'M': 1, 'F': 0})
+        self.df['school'] = self.df['school'].replace({'GP': 1, 'MS': 0})
+        self.df['Pstatus'] = self.df['Pstatus'].replace({'T': 1, 'A': 0})
+        self.df['address'] = self.df['address'].replace({'U': 1, 'R': 0})
+        self.df['subject'] = self.df['subject'].replace({'math': 1, 'portuguese': 0})
+        return self.df
+
+    def var_correlacion(self, columna):
+        """Calcula la correlación de todas las variables numéricas respecto a una variable objetivo específica"""
+        if columna not in self.df.columns:
+            print("Error: La variable '{objetivo}' no existe en el DataFrame.")
+            return None
+
+        correlacion = self.df.corr(numeric_only=True)[columna].sort_values(ascending=False)
+        print(f"--- Correlación con {columna} ---")
+        return correlacion
+
+    def aplicar_dummies(self, columnas_categoricas):
+        """Convierte columnas de texto multiclase en columnas de 1 y 0"""
+        self.df = pd.get_dummies(self.df, columns=columnas_categoricas)
+        return self.df
+
+    def seleccion_de_variables(self, variables_select):
+        """Selecciona del DataFrame solo las columnas elegidas."""
+        self.df = self.df[variables_select]
+        return self.df
+
+                                #*************GRAFICOS***************
+    def grafico_correlación(self):
+        """Genera un grafico de correlación de todas las variables"""
+        corr_matrix = self.df.select_dtypes(include=['number']).corr()
+        # Tamaño de la figura
+        plt.figure(figsize=(15, 10))
+
+        sns.heatmap(corr_matrix,
+                    annot=True,
+                    fmt=".2f",
+                    cmap='coolwarm',
+                    linewidths=0.5)
+
+        plt.title('Matriz de Correlación')
+        plt.show()
+
+    def grafico_boxplot(self, columna_x, columna_y):
+        """Genera un boxplot comparando dos variables"""
+        plt.figure(figsize=(10, 6))
+        sns.boxplot(x=columna_x, y=columna_y, data=self.df)
+        plt.title(f'Relación entre {columna_x} y {columna_y}')
+        plt.show()
+
+
+    def grafico_distribucion(self,columna):
+        """Muestra el histograma y la curva de densidad"""
+        plt.figure(figsize=(12, 8))
+        sns.distplot(self.df[columna])
+
+    def grafico_distribucion02(self, columna):
+        """Muestra el histograma y la curva de densidad"""
+        plt.figure(figsize=(10, 6))
+
+        sns.histplot(self.df[columna], kde=True, color='skyblue', bins=20)
+
+        plt.title(f'Distribución de la variable: {columna}', fontsize=14)
+        plt.xlabel(columna)
+        plt.ylabel('Frecuencia')
+        plt.show()
+
+    def grafico_dispercion(self, columna_x, columna_y):
+        """Crea un gráfico de dispersión para ver la relación entre dos variables"""
+        plt.figure(figsize=(12, 8))
+        sns.scatterplot(x=columna_x, y=columna_y, data=self.df)
+
+
+        #**********************Train and Test split**************************************
+
+    def train_test_x_y(self, val_dropX, val_selectY, test_size=0.2, random_state=101):
+        X = self.df.drop(columns=val_dropX)
+        y = self.df[val_selectY]
+
+        # 1. Aquí guardamos el resultado del split en una variable
+        resultado = train_test_split(X, y, test_size=test_size, random_state=random_state)
+
+        # 2. Devolvemos ESE resultado (que son los 4 DataFrames de Pandas)
+        return resultado
+
+                             #******************Escalado*****************
+
+    def escalado_val(self, X_train, X_test):
+        """Escalado de variables"""
+        scaler = MinMaxScaler()
+        columnas = X_train.columns
+
+        X_train_scaled = scaler.fit_transform(X_train)
+
+        X_test_scaled = scaler.transform(X_test)
+
+        X_train_scaled = pd.DataFrame(X_train_scaled, columns=columnas)
+        X_test_scaled = pd.DataFrame(X_test_scaled, columns=columnas)
+
+        return X_train_scaled, X_test_scaled, scaler
+
+    def guardar_csv(self, datos_df, nombre_archivo, ruta_destino):
+        """ Creacion de archivo .csv"""
+        # Crear carpeta si no existe
+        if not os.path.exists(ruta_destino):
+            os.makedirs(ruta_destino)
+
+        ruta_final = os.path.join(ruta_destino, nombre_archivo)
+
+        # 3. Guardamos
+        datos_df.to_csv(ruta_final, index=False)
+        print(f"Archivo '{nombre_archivo}' guardado en: {ruta_final}")
---
+++ src/train/model1.py
@@ -0,0 +1,93 @@
+from scipy.signal import dfreqresp
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error
+from tensorflow.keras.layers import Dropout
+import pandas as pd
+
+class modelos:
+    def __init__(self, df=None):
+        self.df = df
+
+    def modelo_regresion(self, X_train_df, y_train_df, X_test_df, y_test_df):
+        model = Sequential()
+        num_neuronas = X_train_df.shape[1]
+        model.add(Dense(num_neuronas, activation='relu'))
+        model.add(Dense(num_neuronas, activation='relu'))
+        model.add(Dense(num_neuronas, activation='relu'))
+        model.add(Dense(num_neuronas, activation='relu'))
+        model.add(Dense(1))
+
+        model.compile(optimizer='adam', loss='mse')
+
+        #                           *******Entrenamiento********
+        early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10)
+
+        entrenamiento = model.fit(x=X_train_df, y=y_train_df.values,
+                                  validation_data=(X_test_df, y_test_df.values),
+                                  batch_size=128,
+                                  epochs=200,
+                                  callbacks=[early_stop])
+
+        return entrenamiento, model
+
+
+
+    def create_model_regresion(self, data):
+        model = Sequential()
+        num_neuronas = data.shape[1]
+        model.add(Dense(num_neuronas, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(Dense(num_neuronas, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(Dense(
+            1))
+
+        model.compile(optimizer='adam', loss='mse')
+        return model
+
+    def entrenamiento_model_regresion(self,model,X_train_df, y_train_df, X_test_df, y_test_df):
+        early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10)
+
+        entrenamiento = model.fit(x=X_train_df, y=y_train_df.values,
+                                  validation_data=(X_test_df, y_test_df.values),
+                                  batch_size=32,
+                                  epochs=300,
+                                  callbacks=[early_stop])
+
+        return entrenamiento
+
+    def evaluacion_modelo(self, entrenamiento, model, X_test, y_test):
+        # 1. Graficar de las pérdidas
+        losses = pd.DataFrame(entrenamiento.history)
+        losses.plot()
+        plt.title('Historial de Pérdidas (MSE)')
+        plt.xlabel('Épocas')
+        plt.ylabel('Pérdida')
+        plt.show()
+
+        # 2. Realizar predicciones
+        predictions = model.predict(X_test)
+
+        # 3. Calcular métricas
+        mae = mean_absolute_error(y_test, predictions)
+        # Calculamos la media de la columna real (G3)
+        media_g3 = y_test.mean()
+
+        # Si y_test es un DataFrame de una columna, obtenemos el valor escalar
+        if hasattr(media_g3, 'values'):
+            media_g3 = media_g3.values[0]
+
+        error_relativo = mae / media_g3
+
+        # 4. Mostrar resultados
+        print("-" * 30)
+        print(f"Mean Absolute Error (MAE): {mae:.4f}")
+        print(f"Media de G3: {media_g3:.4f}")
+        print(f"Error Relativo (MAE/Media): {error_relativo:.2%}")
+        print("-" * 30)
+
+        return predictions, mae

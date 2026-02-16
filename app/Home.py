673edd8ca2c3066dcbc2b6b2dde2aa1d4205e4df
mmendoza30 <153874403+mmendoza30@users.noreply.github.com>
2026-02-16 06:13:45 -0600

    ft: Cambios de diseño
    Agrego descripciones

--- app/Home.py
+++ app/Home.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 st.set_page_config(page_title="Sistema Predicciones Académicas", layout="wide")
 
-st.sidebar.title("Menú")
+#st.sidebar.title("Menú")
 #opcion = st.sidebar.radio("Opciones", ["Predicciones", "Comparaciones de los modelos", "Acerca del proyecto"])
 
 col1, col2 = st.columns([1, 4])
@@ -11,4 +11,21 @@ img = Image.open("app/img/educacion-futuro-696x464-1.jpg")
 col1.image(img, width=150)
 col2.title("Sistema de predicciones académicas")
 
-st.write("Selecciona una opción en el menú")
+st.markdown("""
+### Objetivo
+
+Este sistema ha sido desarrollado con el objetivo de predecir el desempeño académico de los estudiantes y detectar de manera temprana el riesgo de deserción escolar mediante el uso de modelos de Inteligencia Artificial.
+
+A partir de variables académicas, sociales y demográficas, la plataforma permite estimar la nota final esperada y clasificar el nivel de riesgo, generando alertas tempranas que facilitan la implementación de intervenciones educativas oportunas.""")
+
+st.subheader("Interpretación de Resultados")
+
+st.markdown("""
+### Creado por:
+
+-Victor Rojas Navarro
+
+-Wedell Orozco Gonzalez
+
+-Mónica Mendoza Morales
+""")
